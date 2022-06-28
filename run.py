6a2c0985d83a0a9d368f6aced93b511687eabdf7
zzyyyl <74587068+zzyyyl@users.noreply.github.com>
2022-06-29 02:34:45 +0800

    feat: `run.py` 按照顺序为视频编号

--- run.py
+++ run.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     with open("download.txt", 'r') as f:
         r = f.read()
     r = r.split('\n')
+    cnt = 0
     for x in r:
         if not x:
             continue
-        os.system(f"start /MIN cmd /C python strange.py -u=\"{x}\"")
+        os.system(f"start /MIN cmd /C python strange.py -u=\"{x}\" -o={cnt}")
+        cnt += 1
