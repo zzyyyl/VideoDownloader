10c26ded03da986a40fc8991fb454bb2d6da340a
zzyyyl <74587068+zzyyyl@users.noreply.github.com>
2022-06-29 01:58:34 +0800

    feat: run!

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+
+if __name__ == "__main__":
+    with open("download.txt", 'r') as f:
+        r = f.read()
+    r = r.split('\n')
+    for x in r:
+        if not x:
+            continue
+        os.system(f"start cmd /C python strange.py -u=\"{x}\"")
